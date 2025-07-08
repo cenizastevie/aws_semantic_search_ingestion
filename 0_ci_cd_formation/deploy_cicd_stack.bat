c6abe55c8fb46182fe7c32bc3a93508d43d0059d
ceniza.sjs <cenizastevie@gmail.com>
2025-07-08 15:27:31 +0800

    Implement cloud formation for cicd for the repository.

---
+++ 0_ci_cd_formation/deploy_cicd_stack.bat
@@ -0,0 +1,25 @@
+@echo off
+REM Deploy the CI/CD CloudFormation stack using parameters from .env
+
+REM Prompt for branch name
+set /p BRANCH_NAME=Enter the branch name for this stack (default: main): 
+if "%BRANCH_NAME%"=="" set BRANCH_NAME=main
+
+REM Load environment variables from .env
+for /f "usebackq tokens=1,2 delims==" %%A in (".env") do set %%A=%%B
+
+REM Append branch name to stack name
+set STACK_NAME=%STACK_NAME%-%BRANCH_NAME%
+
+REM Deploy the stack
+aws cloudformation deploy ^
+  --template-file 0_ci_cd_formation.yaml ^
+  --stack-name %STACK_NAME% ^
+  --region %REGION% ^
+  --capabilities CAPABILITY_NAMED_IAM ^
+  --parameter-overrides ^
+    ConnectionArn=%CONNECTION_ARN% ^
+    GitHubUser=%GITHUB_USER% ^
+    GitHubRepo=%GITHUB_REPO% ^
+    BranchName=%BRANCH_NAME% ^
+    AWSAccountId=%AWS_ACCOUNT_ID%
--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Repository of my semantic search app ingestion pipleline.
 
 ```
-aws cloudformation deploy --template-file cloudformation/1_fargate_task_formation.yaml --stack-name assi-fargate-task --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation deploy --template-file 1_fargate_task/1_fargate_task_formation.yaml --stack-name assi-fargate-task-main --capabilities CAPABILITY_NAMED_IAM --parameter-overrides BranchName=main
 ```
